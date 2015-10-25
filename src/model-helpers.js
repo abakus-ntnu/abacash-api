fe1230ded9b9e24b350af6d3c1c51a158ebc717c
ekmartin <mail@ekmartin.no>
2015-10-25 15:54:17 +0100

    Remove extranous logger

--- src/model-helpers.js
+++ src/model-helpers.js
@@ -1,9 +1,7 @@
 import db from './models';
-import config from './config';
 
 export function syncDB({ force } = {}) {
-    const logging = config.nodeEnv === 'development' ? console.log : false;
     return db
         .sequelize
-        .sync({ force, logging });
+        .sync({ force });
 }
