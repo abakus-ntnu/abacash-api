f3ebe6eb69d24fec1a68a048dd57af12a1b05ac5
Håkon Åmdal <hakon@aamdal.com>
2015-10-25 19:04:43 +0100

    Product cleanup. Rewrite list with query params. New test for products of type

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -4,17 +4,8 @@ import * as controller from '../controllers/product.controller';
 const router = express.Router();
 
 router.get('/', controller.list);
-router.get('/all', controller.listAll);
 router.post('/', controller.create);
-router.get('/:id', controller.get);
+router.get('/:id', controller.retrieve);
 router.delete('/:id', controller.destroy);
-/*
-router.post('/:system', controller.create);
-router.get('/:system/all', controller.getAll);
-router.get('/:system/:id', controller.show);
-router.put('/:system/:id', controller.update);
-router.delete('/:system/:id', controller.destroy);
-router.get('/:system/type/:type', controller.type);
-*/
 
 export default router;
