da170ebb0798fc31ea99a0bfd13a5f795f0716f1
Håkon Åmdal <hakon@aamdal.com>
2015-10-25 18:13:08 +0100

    Add product API

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -1,14 +1,19 @@
 import express from 'express';
-import controller from './product.controller';
+import * as controller from '../controllers/product.controller';
 
 const router = express.Router();
 
-router.get('/:system', controller.active);
+router.get('/', controller.list);
+router.get('/all', controller.listAll);
+router.post('/', controller.create);
+router.get('/:id', controller.get);
+/*
 router.post('/:system', controller.create);
-router.get('/:system/all', controller.index);
+router.get('/:system/all', controller.getAll);
 router.get('/:system/:id', controller.show);
 router.put('/:system/:id', controller.update);
 router.delete('/:system/:id', controller.destroy);
 router.get('/:system/type/:type', controller.type);
+*/
 
 export default router;
