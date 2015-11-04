aaf0be2b1f0ed604bb42948ff9a6d968ee542b2d
Håkon Åmdal <hakon@aamdal.com>
2015-11-04 21:29:37 +0100

    Add error log to test

--- src/components/errors.js
+++ src/components/errors.js
@@ -40,7 +40,7 @@ export class NotFoundError extends Error {
 }
 
 export function errorMiddleware(err, req, res, next) {
-    if (process.env.NODE_ENV === 'development') {
+    if (process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test') {
         console.log(err.stack);
     }
 
