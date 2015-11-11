605d227b5d03fa3003b256dea1e331a269db5660
ekmartin <mail@ekmartin.no>
2015-11-11 17:37:32 +0100

    Set sane defaults for customer role

--- src/config.js
+++ src/config.js
@@ -1,7 +1,8 @@
 const config = {
     port: process.env.PORT || 9000,
     pgUrl: process.env.PG_URL || 'postgres://localhost/abacash',
-    nodeEnv: process.env.NODE_ENV || 'production'
+    nodeEnv: process.env.NODE_ENV || 'production',
+    defaultCustomerRole: process.env.DEFAULT_CUSTOMER_ROLE || 'customer'
 };
 
 export default config;
