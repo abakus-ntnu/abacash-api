9246a6a488ef7908d054f710f000373cd09cf3f8
Håkon Åmdal <hakon@aamdal.com>
2015-12-25 23:57:01 +0100

    Use continuation-local-storage for implicit transaction control

--- src/controllers/transaction.controller.js
+++ src/controllers/transaction.controller.js
@@ -38,30 +38,29 @@ export function add(req, res, next) {
         }
 
         // find and store customer
-        return db.Customer.findById(req.body.customerId, { transaction: t })
+        return db.Customer.findById(req.body.customerId)
         .then((customer) => {
             if (!customer) {
                 throw new ValidationError('Customer for transaction not found');
             }
             _customer = customer;
-            return customer.getCustomerRole({ transaction: t });
+            return customer.getCustomerRole();
         })
 
         // find customer role
         .then((customerRole) => {
             _isInternal = customerRole.internalSales;
-            // reduce stock for all products
+            // reduce stock for all products. database transaction must explicitly be
+            // set because a new transaction promise chain implies a new transaction
             return Bluebird.mapSeries(req.body.products, id => {
                 return db.Product.findById(id, { transaction: t })
                 .then(product => {
                     if (product.keepStock) {
                         product.stock--;
                     }
-                    return product.save({
-                        transaction: t 
-                    });
+                    return product.save({ transaction: t });
                 });
-            }, { transaction: t });
+            });
         })
 
         // calculate sum of all products
@@ -74,7 +73,7 @@ export function add(req, res, next) {
                 ...req.body,
                 systemId: req.system.id,
                 total
-            }, { transaction: t });
+            });
         })
 
         // decrease customer balance
@@ -84,7 +83,7 @@ export function add(req, res, next) {
             if (_customer.balance < 0) {
                 throw new ValidationError('Insufficient balance');
             }
-            return _customer.save({ transaction: t });
+            return _customer.save();
         });
     }).then((result) => {
         // entire database transaction OK, return newly created transaction
