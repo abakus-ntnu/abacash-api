e468f2531c5f8d686c60a30d36d70a1fb50586b6
Håkon Åmdal <hakon@aamdal.com>
2016-02-02 20:20:32 +0100

    Rename to load-db and disable in production

--- bin/load-db.js
+++ bin/load-db.js
@@ -4,6 +4,12 @@ import db from '../src/models';
 import sequelizeFixtures from 'sequelize-fixtures';
 import { syncDB } from '../src/model-helpers';
 
+// verify that we are not in production
+if (process.env.NODE_ENV === 'production') {
+    console.log('Unable to load database in production (NODE_ENV==\'production\')'); 
+    process.exit(1);
+}
+
 const fixtureDir = 'fixtures/';
 const fixtures = [
     'systems',
