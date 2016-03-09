204edad0eb7ea26e8693c89ef7e00ab4a5b753b5
Martin Ek <mail@ekmartin.com>
2016-03-09 19:04:29 +0100

    Move auth controller to its own file

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,41 @@
+import _ from 'lodash';
+import jwt from 'jsonwebtoken';
+import { AuthenticationError, ValidationError } from '../components/errors';
+import db from '../models';
+import config from '../config';
+
+export function login(req, res, next) {
+    const { email, password } = req.body;
+    if (!email || !password) {
+        const error = new ValidationError('Authentication requires both email and password');
+        return next(error);
+    }
+
+    const throwAuthError = () => {
+        throw new AuthenticationError('Invalid credentials provided');
+    };
+
+    let _user;
+    db.User.findOne({
+        where: { email }
+    })
+    .then(user => {
+        if (!user) throwAuthError();
+        _user = user.get({ plain: true });
+        return user.authenticate(password);
+    })
+    .then(valid => {
+        if (!valid) throwAuthError();
+        const cleanUser = _.omit(_user, 'hash');
+
+        // Even though jwt has a callback method,
+        // there's no point in using it as the functions
+        // it executes are synchronous:
+        return jwt.sign(cleanUser, config.jwtSecret, {
+            expiresIn: config.jwtExpiresIn,
+            subject: _user.id
+        });
+    })
+    .then(token => res.json({ token }))
+    .catch(next);
+}
