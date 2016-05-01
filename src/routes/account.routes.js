92e4294d77d236450490962f09948f22d03f2e0b
Håkon Åmdal <hakon@aamdal.com>
2016-05-01 15:04:07 +0200

    Use new authentication in account router

--- src/routes/account.routes.js
+++ src/routes/account.routes.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import * as controller from '../controllers/account.controller';
-import { isAuthenticated } from '../auth/middleware';
+import { requires } from '../auth';
+import { MODERATOR } from '../auth/constants';
 
 const router = express.Router();
 
-router.use(isAuthenticated);
+router.use(requires(MODERATOR));
 router.get('/', controller.retrieve);
 
 export default router;
