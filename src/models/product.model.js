20e5271b31312c58921870173fec40450f90eb5d
Kristoffer K Larsen <kristoffer@larsen.so>
2017-01-28 11:38:42 +0100

    Updated product model

--- src/models/product.model.js
+++ src/models/product.model.js
@@ -17,7 +17,10 @@ export default function(sequelize, DataTypes) {
                 return Number(this.getDataValue('internalPrice'));
             }
         },
-        name: DataTypes.STRING,
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
         active: {
             type: DataTypes.BOOLEAN,
             defaultValue: false
