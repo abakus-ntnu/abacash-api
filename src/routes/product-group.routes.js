76b57bed027fb3cb16bd24f9bb6fef4011fd9ebd
Eirik Martiniussen Sylliaas <eirik@sylliaas.no>
2017-02-07 20:50:25 +0100

    Change product group api authentication

--- src/routes/product-group.routes.js
+++ src/routes/product-group.routes.js
@@ -1,14 +1,13 @@
 import express from 'express';
 import * as controller from '../controllers/product-group.controller';
 import { createAuthMiddleware } from '../auth';
-import { MODERATOR } from '../auth/constants';
+import { MODERATOR, TOKEN } from '../auth/constants';
 
 const router = express.Router();
 
-router.use(createAuthMiddleware(MODERATOR));
-router.get('/', controller.list);
-router.post('/', controller.create);
-router.put('/:id', controller.update);
-router.delete('/:id', controller.destroy);
+router.get('/', createAuthMiddleware(TOKEN), controller.list);
+router.post('/', createAuthMiddleware(MODERATOR), controller.create);
+router.put('/:id', createAuthMiddleware(MODERATOR), controller.update);
+router.delete('/:id', createAuthMiddleware(MODERATOR), controller.destroy);
 
 export default router;
