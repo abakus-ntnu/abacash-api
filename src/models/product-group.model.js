538809c4470f06c8e0f88624cdef74ec173e6385
larseen <kristoffer@larsen.so>
2018-02-20 16:54:38 +0100

    Upgrade product models

--- src/models/product-group.model.js
+++ src/models/product-group.model.js
@@ -1,20 +1,14 @@
 export default function(sequelize, DataTypes) {
-  const ProductGroup = sequelize.define(
-    'productGroup',
-    {
-      name: {
-        type: DataTypes.STRING,
-        allowNull: false
-      }
-    },
-    {
-      classMethods: {
-        associate(models) {
-          ProductGroup.hasMany(models.Product);
-        }
-      }
+  const ProductGroup = sequelize.define('productGroup', {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
     }
-  );
+  });
+
+  ProductGroup.associate = function(models) {
+    ProductGroup.hasMany(models.Product);
+  };
 
   return ProductGroup;
 }
--- src/models/product.model.js
+++ src/models/product.model.js
@@ -1,51 +1,45 @@
 export default function(sequelize, DataTypes) {
-  const Product = sequelize.define(
-    'product',
-    {
-      price: {
-        type: DataTypes.DECIMAL,
-        get() {
-          return Number(this.getDataValue('price'));
-        }
-      },
-      internalPrice: {
-        type: DataTypes.DECIMAL,
-        allowNull: true,
-        get() {
-          if (!this.getDataValue('internalPrice')) {
-            return Number(this.getDataValue('price'));
-          }
-          return Number(this.getDataValue('internalPrice'));
-        }
-      },
-      name: {
-        type: DataTypes.STRING,
-        allowNull: false
-      },
-      active: {
-        type: DataTypes.BOOLEAN,
-        defaultValue: false
-      },
-      stock: {
-        type: DataTypes.INTEGER,
-        defaultValue: 0
-      },
-      keepStock: {
-        type: DataTypes.BOOLEAN,
-        defaultValue: false
+  const Product = sequelize.define('product', {
+    price: {
+      type: DataTypes.DECIMAL,
+      get() {
+        return Number(this.getDataValue('price'));
       }
     },
-    {
-      classMethods: {
-        associate(models) {
-          Product.belongsTo(models.ProductGroup);
-          Product.belongsToMany(models.Transaction, {
-            through: models.TransactionProduct
-          });
+    internalPrice: {
+      type: DataTypes.DECIMAL,
+      allowNull: true,
+      get() {
+        if (!this.getDataValue('internalPrice')) {
+          return Number(this.getDataValue('price'));
         }
+        return Number(this.getDataValue('internalPrice'));
       }
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    active: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0
+    },
+    keepStock: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false
     }
-  );
+  });
+
+  Product.associate = function(models) {
+    Product.belongsTo(models.ProductGroup);
+    Product.belongsToMany(models.Transaction, {
+      through: models.TransactionProduct
+    });
+  };
 
   return Product;
 }
