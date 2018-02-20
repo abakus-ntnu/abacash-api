33d66ae6dcb7054bc3e5fab3ffbcc0b00cbc86bc
larseen <kristoffer@larsen.so>
2018-02-20 16:58:34 +0100

    Upgrade transactions models

--- src/models/transaction.model.js
+++ src/models/transaction.model.js
@@ -1,43 +1,37 @@
 export default function(sequelize, DataTypes) {
-  const Transaction = sequelize.define(
-    'transaction',
-    {
-      total: {
-        type: DataTypes.DECIMAL,
-        defaultValue: 0.0,
-        get() {
-          return Math.round(Number(this.getDataValue('total')) * 100) / 100.0;
-        }
-      },
-      customerId: {
-        type: DataTypes.INTEGER,
-        allowNull: false
-      },
-      sellerId: {
-        type: DataTypes.INTEGER,
-        allowNull: true
+  const Transaction = sequelize.define('transaction', {
+    total: {
+      type: DataTypes.DECIMAL,
+      defaultValue: 0.0,
+      get() {
+        return Math.round(Number(this.getDataValue('total')) * 100) / 100.0;
       }
     },
-    {
-      classMethods: {
-        associate(models) {
-          Transaction.belongsToMany(models.Product, {
-            through: models.TransactionProduct
-          });
-
-          Transaction.belongsTo(models.Customer, {
-            as: 'customer',
-            foreignKey: 'customerId'
-          });
-
-          Transaction.belongsTo(models.Customer, {
-            as: 'seller',
-            foreignKey: 'sellerId'
-          });
-        }
-      }
+    customerId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    sellerId: {
+      type: DataTypes.INTEGER,
+      allowNull: true
     }
-  );
+  });
+
+  Transaction.associate = function(models) {
+    Transaction.belongsToMany(models.Product, {
+      through: models.TransactionProduct
+    });
+
+    Transaction.belongsTo(models.Customer, {
+      as: 'customer',
+      foreignKey: 'customerId'
+    });
+
+    Transaction.belongsTo(models.Customer, {
+      as: 'seller',
+      foreignKey: 'sellerId'
+    });
+  };
 
   return Transaction;
 }
--- src/models/transactionProduct.model.js
+++ src/models/transactionProduct.model.js
@@ -1,40 +1,5 @@
 import db from '.';
 
-const trackProductUsage = transactionProduct => {
-  let product = null;
-  let customer = null;
-  let transaction = null;
-
-  db.Product.findOne({
-    where: {
-      id: transactionProduct.productId
-    }
-  })
-    .then(result => {
-      product = result;
-      return result;
-    })
-    .then(() =>
-      db.Transaction.findOne({
-        where: {
-          id: transactionProduct.transactionId
-        }
-      })
-    )
-    .then(result => {
-      if (result) {
-        transaction = result;
-        return result;
-      }
-      return Promise.resolve();
-    })
-    .then(() => transaction.getCustomer())
-    .then(result => {
-      customer = result;
-      return result;
-    });
-};
-
 export default function(sequelize, DataTypes) {
   return sequelize.define('transactionProduct', {
     count: {
